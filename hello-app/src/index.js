60457039c5057a1e04beff1580cd98f4e1e045ca
ratnappatel <ratnappatel@gmail.com>
2022-10-08 11:37:23 +0530

    Components

---
+++ hello-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// used to render any HTML content as output on Single page of React application
+ReactDOM.render(
+    <div>
+        <html>
+            <body>
+                    <h3>
+                        Hello From React JS Family 
+                    </h3>
+                    <p>My Test Paragraph</p>
+            </body>
+         </html>
+          
+
+    </div>
+    ,
+    document.getElementById('root')
+);
