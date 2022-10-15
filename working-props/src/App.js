c78009b539862f26fbf72e8f4028210f603c2661
ratnappatel <ratnappatel@gmail.com>
2022-10-15 10:55:18 +0530

    Combined Code

--- working-props/src/App.js
+++ working-props/src/App.js
@@ -1,8 +1,4 @@
-
-import './App.css';
-
-import Employees from './Employees';
-
+import React, {Component} from 'react';
 function App() {
   return (
        <div>
@@ -10,5 +6,38 @@ function App() {
        </div>
       );
 }
+
+
+
+class Employees extends Component{
+  render(){
+      return(
+          <div>
+              <Employee name="Raj" age="29" workLocation="Delhi"></Employee>
+              <Employee name="Uttam" age="32" workLocation="Ahmedabad"></Employee>
+              <Employee name="Neel" age="28" ></Employee>
+              <Employee name="Gargesh" age="22" workLocation="NCR"></Employee>
+          </div>
+      );
+  }
+}
+
+
+class Employee extends Component{
+  render(){
+      return(
+          <div>
+              <p>Name : {this.props.name}</p>
+              <p>Age : {this.props.age}</p>
+              <p>Work Location: {this.props.workLocation}</p>
+              <hr height="5"></hr>
+          </div>
+      );
+  }
+}
+
+Employee.defaultProps={
+  workLocation: 'Bangaluru'
+};
 
 export default App;
